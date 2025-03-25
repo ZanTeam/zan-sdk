f8dec25d26ddb625be653aea721dad73835b0221
gaoyizhuo.gyz <gaoyizhuo.gyz@antgroup.com>
2025-03-25 14:15:06 +0800

    chore: init tools

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { Core } from "./core";
 import { Solana } from "./solana";
 import * as viem from "viem";
-// eslint-disable-next-line @nx/enforce-module-boundaries
 import * as solanaWeb3 from "@solana/web3.js";
 
 export { Core, Solana, viem, solanaWeb3 };
